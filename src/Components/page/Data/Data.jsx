73dcb47e095843e91e7a175f13f9174f41840ec4
jc4milo <jc4milo311@gmail.com>
2023-03-27 21:15:19 -0500

    Feat: Actualización Vesrion 1.3.1

--- src/Components/page/Data/Data.jsx
+++ src/Components/page/Data/Data.jsx
@@ -1,12 +1,15 @@
 import Logo from "../../Images/Interface/Logo.png";
 import { Footer } from "../../UI/Footer/Footer";
 import { Waves } from "../../UI/Waves/Waves";
-import { Link } from "react-router-dom";
 import "./Data.css"
 
 export const Data = (props) => {
 
-
+    const Delecte = () => {
+        localStorage.removeItem("CodePs")
+        localStorage.removeItem("CodeVerifycation")
+        localStorage.removeItem("idCode")
+    }
 
     return (
         <>
@@ -15,7 +18,7 @@ export const Data = (props) => {
                     <div className="Counter_header_2" >
                         <img src={Logo} alt="Logo2" srcSet="" className="Logo" />
                         <h1 className="Text_major">Bienvenido [Nombre]</h1>
-                        <Link to="/login_Supervisor/r/owncaso" className="text"><h1 className="Text_major">Cerrar Sesión</h1></Link>
+                        <a href="/login/r/owncaso" className="text"><h1 onClick={Delecte} className="Text_major">Cerrar Sesión</h1></a>
                     </div>
                 </div>
                 <Waves />
